7829ae50b561f720288dfe988d09da3b53a6ced8
Tunahan Erdem <ttunahanerdemm@gmail.com>
2024-12-15 01:42:28 +0300

    sample project fundamental

--- src/main.js
+++ src/main.js
@@ -4,30 +4,10 @@ import { movielist } from './js/movielist';
 import { upcoming } from './js/upcoming';
 import { footer } from './js/footer';
 import theme from './js/theme';
-import { mylibraryUpdate } from './js/mylibraryUpdate';
 
-if (window.location.pathname.includes('catalog.html')) {
-  header();
-  hero();
-  movielist();
-  footer();
-  theme();
-}
-if (window.location.pathname.includes('mylibrary.html')) {
-  header();
-  hero();
-  footer();
-  theme();
-  mylibraryUpdate();
-}
-if (
-  !window.location.pathname.includes('mylibrary.html') &&
-  !window.location.pathname.includes('catalog.html')
-) {
-  header();
-  hero();
-  upcoming();
-  movielist();
-  footer();
-  theme();
-}
+header();
+hero();
+movielist();
+upcoming();
+footer();
+theme();
